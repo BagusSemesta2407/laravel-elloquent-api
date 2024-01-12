4877e94d26628fc61d37d3616148bd957e7f9d5e
BagusSemesta2407 <semestabagus24@gmail.com>
2024-01-12 14:35:59 +0700

    second commit

---
+++ tests/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    protected function setUp() :void 
+    {
+        parent::setUp();
+
+        // DB::delete("delete from products");
+        // DB::delete("delete from categories");
+    }
+}
